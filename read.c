fe0319b1e99f79b2eac02254e1cad2138aa64f55
Adam <Adam_Wasilewski_99@proton.me>
2025-10-09 21:48:43 +0200

    read_files

---
+++ read.c
@@ -0,0 +1,12 @@
+#include "read.h"
+void read_inputs(){
+  tv_code_P.whl_speed_fl_Value = 0;
+  tv_code_P.whl_speed_fr_Value = 0;
+  tv_code_P.whl_speed_rl_Value = 0;
+  tv_code_P.whl_speed_rr_Value = 0;
+  tv_code_P.delta_Value = 0;
+  tv_code_P.acc_pedal_Value = 0; 
+  tv_code_P.ax_Value = 0;
+  tv_code_P.ay_Value = 0;
+  tv_code_P.yaw_rate_Value = 0;
+}
---
+++ read.h
@@ -0,0 +1,4 @@
+#ifndef read_h_
+#include "tv_code.h"
+void read_inputs();
+#endif
