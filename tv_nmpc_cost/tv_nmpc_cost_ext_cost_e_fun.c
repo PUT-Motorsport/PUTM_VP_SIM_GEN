791ee07a4d336695ebefef875e683e352e1a596c
fmichalski03 <filip.michalski.1@student.put.poznan.pl>
2026-07-05 03:40:04 +0200

    New NMPC

--- tv_nmpc_cost/tv_nmpc_cost_ext_cost_e_fun.c
+++ tv_nmpc_cost/tv_nmpc_cost_ext_cost_e_fun.c
@@ -73,7 +73,7 @@ static const casadi_real casadi_zeros[11] =
 
 /* tv_nmpc_cost_ext_cost_e_fun:(i0[11],i1[8])->(o0) */
 static int casadi_f0(const casadi_real** arg, casadi_real** res, casadi_int* iw, casadi_real* w, int mem) {
-  casadi_real w0, w2, w4, w5;
+  casadi_real w0, w2, w4, w5, w6;
   const casadi_real *wr1, *wr3;
   /* #0: @0 = 1000 */
   w0 = 1000.;
@@ -91,23 +91,25 @@ static int casadi_f0(const casadi_real** arg, casadi_real** res, casadi_int* iw,
   w2 = casadi_sq( w2 );
   /* #7: @0 = (@0*@2) */
   w0 *= w2;
-  /* #8: @2 = @1[1] */
-  w2 = wr1[1];
-  /* #9: @4 = @1[0] */
-  w4 = wr1[0];
-  /* #10: @5 = 0.5 */
-  w5 = 5.0000000000000000e-01;
-  /* #11: @4 = fmax(@4,@5) */
-  w4  = casadi_fmax(w4,w5);
-  /* #12: @2 = (@2/@4) */
-  w2 /= w4;
-  /* #13: @2 = sq(@2) */
-  w2 = casadi_sq( w2 );
-  /* #14: @2 = (2.*@2) */
-  w2 = (2.* w2 );
-  /* #15: @0 = (@0+@2) */
+  /* #8: @2 = 50 */
+  w2 = 50.;
+  /* #9: @4 = @1[1] */
+  w4 = wr1[1];
+  /* #10: @5 = @1[0] */
+  w5 = wr1[0];
+  /* #11: @6 = 0.5 */
+  w6 = 5.0000000000000000e-01;
+  /* #12: @5 = fmax(@5,@6) */
+  w5  = casadi_fmax(w5,w6);
+  /* #13: @4 = (@4/@5) */
+  w4 /= w5;
+  /* #14: @4 = sq(@4) */
+  w4 = casadi_sq( w4 );
+  /* #15: @2 = (@2*@4) */
+  w2 *= w4;
+  /* #16: @0 = (@0+@2) */
   w0 += w2;
-  /* #16: output[0][0] = @0 */
+  /* #17: output[0][0] = @0 */
   if (res[0]) res[0][0] = w0;
   return 0;
 }
@@ -184,7 +186,7 @@ CASADI_SYMBOL_EXPORT int tv_nmpc_cost_ext_cost_e_fun_work(casadi_int *sz_arg, ca
   if (sz_arg) *sz_arg = 4;
   if (sz_res) *sz_res = 2;
   if (sz_iw) *sz_iw = 0;
-  if (sz_w) *sz_w = 23;
+  if (sz_w) *sz_w = 24;
   return 0;
 }
 
@@ -192,7 +194,7 @@ CASADI_SYMBOL_EXPORT int tv_nmpc_cost_ext_cost_e_fun_work_bytes(casadi_int *sz_a
   if (sz_arg) *sz_arg = 4*sizeof(const casadi_real*);
   if (sz_res) *sz_res = 2*sizeof(casadi_real*);
   if (sz_iw) *sz_iw = 0*sizeof(casadi_int);
-  if (sz_w) *sz_w = 23*sizeof(casadi_real);
+  if (sz_w) *sz_w = 24*sizeof(casadi_real);
   return 0;
 }
 
